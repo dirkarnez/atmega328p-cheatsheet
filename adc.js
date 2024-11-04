ac757d951e0e3af1eb3f0b2df034f5396c07203c
Dirk Arnez <smalldirkalex@gmail.com>
2024-11-05 01:56:03 +0800

    - update adc.js

--- adc.js
+++ adc.js
@@ -1,7 +1,25 @@
 void adc_init()
 {
-	ADMUX = 0b01000000;
-	ADCSRA |= (1 << ADEN) | (0 << ADPS2) | (1 << ADPS1) | (0 << ADPS0);
+	// ADMUX = 0b01000000;
+	// ADCSRA |= (1 << ADEN) | (0 << ADPS2) | (1 << ADPS1) | (0 << ADPS0);
+	ADMUX = 
+	(1 << REFS1 ) | // 7
+	(1 << REFS0 ) | // 6
+	(1 << ADLAR ) | // 5, nothing on 4
+	(1 << MUX3 ) | // 3
+	(1 << MUX2 ) | // 2
+	(1 << MUX1 ) | // 1
+	(1 << MUX0 ); // 0
+
+	ADCSRA = 
+	( 1 << ADEN  ) | // 7
+	( 1 << ADSC  ) | // 6
+	( 1 << ADATE ) | // 5
+	( 1 << ADIF  ) | // 4
+	( 1 << ADIE  ) | // 3
+	( 1 << ADPS2 ) | // 2
+	( 1 << ADPS1 ) | // 1
+	( 1 << ADPS0 ); // 0
 }
 
 int adc_read(unsigned char channel)
